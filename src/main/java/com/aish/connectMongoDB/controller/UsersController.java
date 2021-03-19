b589b31cefb99f4478b05b7272a2e5e5ec6ca798
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-03-19 12:41:11 +0530

    changed in login functionality with token send whole data

--- src/main/java/com/aish/connectMongoDB/controller/UsersController.java
+++ src/main/java/com/aish/connectMongoDB/controller/UsersController.java
@@ -21,38 +21,58 @@ public class UsersController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private JwtUtil jwtUtil;
-	
+
 	@Autowired
 	private UsersLoginService usersLoginService;
-	
+
+	/**
+	 * add new user
+	 * 
+	 * @param userToAdd
+	 * @return
+	 */
 	@PostMapping("/addUser")
-	public ResponseEntity<JwtTokenWithUser> addUser(@RequestBody Users userToAdd){
-		
+	public ResponseEntity<JwtTokenWithUser> addUser(@RequestBody Users userToAdd) {
+
 		// save new user
-		Users addedUser=userRepository.save(userToAdd);
-		
-		//generate token using user name
-		final String jwtToken =jwtUtil.generateTokenUsingUserName(addedUser.getUserName());
-		
-		//generate token with user
-		JwtTokenWithUser jwtTokenWithUser=new JwtTokenWithUser(addedUser,jwtToken);
-		return ResponseEntity.ok(jwtTokenWithUser);
+		Users addedUser = userRepository.save(userToAdd);
+
+		// generate token using user name
+		final String jwtToken = jwtUtil.generateTokenUsingUserName(addedUser.getUserName());
+
+		// generate token with user
+		JwtTokenWithUser jwtTokenWithUser = new JwtTokenWithUser(addedUser, jwtToken);
+
+		return new ResponseEntity<JwtTokenWithUser>(jwtTokenWithUser, HttpStatus.OK);
 	}
-	
+
+	/**
+	 * login user with passing userName and password
+	 * 
+	 * @param jwtRequest
+	 * @return
+	 */
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody JwtRequest jwtRequest){
-		Users loggedInUser=usersLoginService.signIn(jwtRequest.getUserName(), jwtRequest.getPassword());
-		
-		
-		
-		if(loggedInUser!=null) {
-			final String jwtToken =jwtUtil.generateTokenUsingUserName(loggedInUser.getUserName());
-			return ResponseEntity.ok(jwtToken);
-		}else {
-			return new ResponseEntity<String>("Invalid email or password", HttpStatus.UNAUTHORIZED);
+	public ResponseEntity<JwtTokenWithUser> login(@RequestBody JwtRequest jwtRequest) {
+
+		// check user is valid or not.
+		Users loggedInUser = usersLoginService.signIn(jwtRequest.getUserName(), jwtRequest.getPassword());
+
+		if (loggedInUser != null) {
+
+			// generate token
+			final String jwtToken = jwtUtil.generateTokenUsingUserName(loggedInUser.getUserName());
+
+			// with passing loggedUser and token fetch jwtTokenWithUser.
+			JwtTokenWithUser jwtTokenWithUser = new JwtTokenWithUser(loggedInUser, jwtToken);
+
+			return new ResponseEntity<JwtTokenWithUser>(jwtTokenWithUser, HttpStatus.OK);
+
 		}
+		return new ResponseEntity<JwtTokenWithUser>(new JwtTokenWithUser(null, null), HttpStatus.UNAUTHORIZED);
+
 	}
 }
--- src/main/java/com/aish/connectMongoDB/jwtUtil/JwtUtil.java
+++ src/main/java/com/aish/connectMongoDB/jwtUtil/JwtUtil.java
@@ -12,35 +12,37 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+
 @Service
 public class JwtUtil {
 
 	private String SECRET_KEY = "secret";
 
+	/**
+	 * generate token
+	 * 
+	 * @param claims
+	 * @param subject
+	 * @return
+	 */
+	public String generateTokenUsingUserName(String userName) {
 
-    /**
-     * generate token 
-     * @param claims
-     * @param subject
-     * @return
-     */
-    public String generateTokenUsingUserName(String userName) {
+		return Jwts.builder().setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+				.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+	}
 
-        return Jwts.builder().setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-    }
-
-    /**
-     * to check token is validate or not and return token with data.
-     * @param token
-     * @param authenticationRequest
-     * @return
-     */
-    public String isValidTokenWithData(String token) throws ExpiredJwtException, UnsupportedJwtException,
-	MalformedJwtException, SignatureException, IllegalArgumentException {
-    	String tokenData = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+	/**
+	 * to check token is validate or not and return token with data.
+	 * 
+	 * @param token
+	 * @param authenticationRequest
+	 * @return
+	 */
+	public String isValidTokenWithData(String token) throws ExpiredJwtException, UnsupportedJwtException,
+			MalformedJwtException, SignatureException, IllegalArgumentException {
+		String tokenData = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
 		return tokenData;
-    }
-	
+	}
+
 }
